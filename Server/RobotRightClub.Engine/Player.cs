657014a51b40109e18dbaeb1c781f42866387411
Pete Fullergreen <pete@pagan-gerbil.net>
2020-06-24 14:09:21 +0100

    Basic objects for a game session

---
+++ Server/RobotRightClub.Engine/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotRightClub.Engine
+{
+    public class Player
+    {
+        public string Name { get; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
