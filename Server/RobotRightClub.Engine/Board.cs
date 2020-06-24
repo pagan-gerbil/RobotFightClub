0e161934411ce7e8a21d002b961e0f33a00a4b1e
Pete Fullergreen <pete@pagan-gerbil.net>
2020-06-24 12:57:48 +0100

    Adding simple objects for board spaces

---
+++ Server/RobotRightClub.Engine/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotRightClub.Engine
+{
+    public class Board
+    {
+        private BoardSpace[,] _spaces = new BoardSpace[10, 6];
+
+        public Board()
+        {
+            SetupSpaces();
+        }
+
+        private void SetupSpaces()
+        {
+            // move this into 'setup cards' later
+
+            
+        }
+
+        public BoardSpace GetSpace(int row, int column)
+        {
+            return _spaces[row, column];
+        }
+    }
+}
---
+++ Server/RobotRightClub.Engine/BoardToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotRightClub.Engine
+{
+    public enum BoardToken
+    {
+        ActivationPad,
+        RobotStart,
+        Obstacle
+    }
+}
