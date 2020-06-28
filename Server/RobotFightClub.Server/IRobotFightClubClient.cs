db90b57dd3f4168afb86799c5da1ae8a7cc5186a
Pete Fullergreen <pete@pagan-gerbil.net>
2020-06-28 15:22:57 +0100

    Adding SignalR server project

---
+++ Server/RobotFightClub.Server/IRobotFightClubClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobotFightClub.Server
+{
+    public interface IRobotFightClubClient
+    {
+    }
+}
